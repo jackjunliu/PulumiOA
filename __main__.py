7c394a38ece2371b189e549d8bd7dc6d6c894c17
Jack Liu <jacliu@paypal.com>
2024-01-16 11:48:02 -0800

    Correcting the main python file

--- __main__.py
+++ __main__.py
@@ -1,102 +1,85 @@
 import pulumi
-import pulumi_kubernetes as kubernetes
+import pulumi_aws as aws
+# https://github.com/pulumi/pulumi-docker
+import pulumi_docker as docker # https://www.pulumi.com/registry/packages/docker/
+import pulumi_kubernetes as k8s
 
-# Get some values from the Pulumi stack configuration, or use defaults
-config = pulumi.Config()
-k8sNamespace = config.get("namespace", "default")
-numReplicas = config.get_float("replicas", 1)
-app_labels = {
-    "app": "nginx",
-}
+# # Fetch the default VPC because we cannot connect directly to the EKS cluster
+# # This is because bridge networks (for DNS) don't integrate well with EKS, which a vpc does
+# default_vpc = aws.ec2.get_vpc(default=True)
 
-# Create a namespace
-webserverns = kubernetes.core.v1.Namespace(
-    "webserver",
-    metadata=kubernetes.meta.v1.ObjectMetaArgs(
-        name=k8sNamespace,
+# # Get all subnets to map VPC to the docker network
+# # https://www.pulumi.com/registry/packages/aws/api-docs/ec2/getsubnets/
+# all_subnets = aws.ec2.get_subnets()
+# subnets = all_subnets.ids
+
+vpc = aws.ec2.Vpc("vpc", cidr_block="172.31.0.0/16")
+subnet = aws.ec2.Subnet("subnet", vpc_id=vpc.id, cidr_block="172.31.1.0/24")
+
+# Create an AWS EKS cluster
+# IAM roles here are written so that worker nodes can figure out which EKS API to connect to
+eks_cluster = aws.eks.Cluster("eksCluster", 
+    role_arn=aws.iam.Role("eksRole",
+        assume_role_policy='''{
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {
+                        "Service": "eks.amazonaws.com"
+                    },
+                    "Action": "sts:AssumeRole"
+                }
+            ]
+        }''').arn,
+    vpc_config=aws.eks.ClusterVpcConfigArgs(
+        # subnet_ids=subnets,  # Use discovered subnets here
+        vpc_id=vpc.id,
+        subnet_ids=[subnet.id],
+        public_access_cidrs=["0.0.0.0/0"],
+        endpoint_public_access=True,
     )
 )
 
-# Create a ConfigMap for the Nginx configuration
-webserverconfig = kubernetes.core.v1.ConfigMap(
-    "webserverconfig",
-    metadata=kubernetes.meta.v1.ObjectMetaArgs(
-        namespace=webserverns.metadata.name,
-    ),
-    data={
-        "nginx.conf": """events { }
-http {
-  server {
-    listen 80;
-    root /usr/share/nginx/html;
-    index index.html index.htm index.nginx-debian.html
-    server_name _;
-    location / {
-      try_files $uri $uri/ =404;
-    }
-  }
-}
-""",
-    }
+# Define the Docker build for the static website
+static_website_image = docker.Image("staticWebsiteImage",
+  build=docker.DockerBuildArgs(
+    context=".",
+    dockerfile="Dockerfile"
+  ),
+  image_name="my-static-website",
+  skip_push=False  
 )
 
-# Create a Deployment with a user-defined number of replicas
-webserverdeployment = kubernetes.apps.v1.Deployment(
-    "webserverdeployment",
-    metadata=kubernetes.meta.v1.ObjectMetaArgs(
-        namespace=webserverns.metadata.name,
+# # Reference pre-built Docker image
+# static_website_image = docker.Image("staticWebsiteImage",
+#   image_name="my-static-website:v1.0.0"  
+# )
+
+# Deploy the website as a Kubernetes Deployment
+app_labels = {"app": "static-website"}
+static_website_deployment = k8s.apps.v1.Deployment("staticWebsiteDeployment",
+    metadata=k8s.meta.v1.ObjectMetaArgs(
+        labels=app_labels,
+        namespace="default",
     ),
-    spec=kubernetes.apps.v1.DeploymentSpecArgs(
-        selector=kubernetes.meta.v1.LabelSelectorArgs(
-            match_labels=app_labels,
-        ),
-        replicas=numReplicas,
-        template=kubernetes.core.v1.PodTemplateSpecArgs(
-            metadata=kubernetes.meta.v1.ObjectMetaArgs(
-                labels=app_labels,
-            ),
-            spec=kubernetes.core.v1.PodSpecArgs(
-                containers=[kubernetes.core.v1.ContainerArgs(
-                    image="nginx",
-                    name="nginx",
-                    volume_mounts=[kubernetes.core.v1.VolumeMountArgs(
-                        mount_path="/etc/nginx/nginx.conf",
-                        name="nginx-conf-volume",
-                        read_only=True,
-                        sub_path="nginx.conf",
-                    )],
-                )],
-                volumes=[kubernetes.core.v1.VolumeArgs(
-                    config_map=kubernetes.core.v1.ConfigMapVolumeSourceArgs(
-                        items=[kubernetes.core.v1.KeyToPathArgs(
-                            key="nginx.conf",
-                            path="nginx.conf",
-                        )],
-                        name=webserverconfig.metadata.name,
-                    ),
-                    name="nginx-conf-volume",
+    spec=k8s.apps.v1.DeploymentSpecArgs(
+        replicas=1,
+        selector=k8s.meta.v1.LabelSelectorArgs(match_labels=app_labels),
+        template=k8s.core.v1.PodTemplateSpecArgs(
+            metadata=k8s.meta.v1.ObjectMetaArgs(labels=app_labels),
+            spec=k8s.core.v1.PodSpecArgs(
+                containers=[k8s.core.v1.ContainerArgs(
+                    name="static-website-container",
+                    image=static_website_image.image_name,
                 )],
             ),
         ),
-    )
-)
-
-# Expose the Deployment as a Kubernetes Service
-webserverservice = kubernetes.core.v1.Service(
-    "webserverservice",
-    metadata=kubernetes.meta.v1.ObjectMetaArgs(
-        namespace=webserverns.metadata.name,
     ),
-    spec=kubernetes.core.v1.ServiceSpecArgs(
-        ports=[kubernetes.core.v1.ServicePortArgs(
-            port=80,
-            target_port=80,
-            protocol="TCP",
-        )],
-        selector=app_labels,
+    opts=pulumi.ResourceOptions(
+        depends_on=[eks_cluster]
     )
 )
 
-# Export some values for use elsewhere
-pulumi.export("deploymentName", webserverdeployment.metadata.name)
-pulumi.export("serviceName", webserverservice.metadata.name)
+# Export the EKS cluster kubeconfig
+pulumi.export("kubeconfig", eks_cluster.kubeconfig)
